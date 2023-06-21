855e25db894f8710676aa2a5a3afd75b423bacd3
Szczepan <mojemail@gmail.com>
2023-06-21 09:26:43 +0200

    start.bat

---
+++ start.bat
@@ -0,0 +1,9 @@
+%systemDrive%\xampp\mysql\bin\mysql -uroot -e "CREATE DATABASE IF NOT EXISTS spa;"
+php -r "copy('.env.example', '.env');"
+call composer install
+call php artisan key:generate
+call php artisan storage:link
+call php artisan migrate:fresh
+call php artisan db:seed
+start http://127.0.0.1:8000
+call php artisan serve
